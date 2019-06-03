e57ebe65001795c91862d6b4381d2b5016717885
Luc Kuenemann <kuenemann.luc@protonmail.com>
2019-06-03 13:43:09 +0200

    Implemented linked list of char "charlist" type

--- source/shamitab.h
+++ source/shamitab.h
@@ -13,6 +13,11 @@ typedef int32_t symbol;	// Defining a 32 bits type for a 33t symbol
 const int symbol_size = 4;
 const symbol eof = 0xffffffff;
 
+typedef struct CHARLIST {
+	char c;
+	struct CHARLIST* next;
+} charlist;
+
 
 int main(int argc, char* argv[]);
 
