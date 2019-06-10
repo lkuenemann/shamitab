b68c9c626d965cf9da532fa4cc3528227b17ac5f
Luc Kuenemann <kuenemann.luc@protonmail.com>
2019-06-10 17:15:48 +0200

    Added test for all finger markers

---
+++ test/test-all-fingers.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -eu
+
+#Define tab viewer ASCII output file and correct expected output file
+SHAMITAB='../shamitab'
+TAB='all-fingers.3mt'
+OUTPUT='output-tab.txt'
+CORRECT='all-fingers.txt'
+
+#Check that both files exist
+if [ ! -f $TAB ]; then
+  >&2 echo "ERROR: tab file $TAB not found!";
+  exit 1;
+fi
+if [ ! -f $CORRECT ]; then
+  >&2 echo "ERROR: correction file $CORRECT not found!";
+  exit 1;
+fi
+
+#Call Shamitab on the tab
+$SHAMITAB $TAB > $OUTPUT
+
+#Compare files
+if diff --brief $OUTPUT $CORRECT > /dev/null;
+  then echo "PASS"
+  else echo "FAIL"
+fi
+
+rm $OUTPUT
