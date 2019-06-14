1b3a467f025a42f8edab72837103755ab0dc3ea0
Luc Kuenemann <kuenemann.luc@protonmail.com>
2019-06-14 17:44:24 +0200

    Added command parsing for view, convert, and help Added help command Tab viewing is now called with command "view"

--- source/shamitab.c
+++ source/shamitab.c
@@ -6,6 +6,8 @@
 
 #include "view.h"
 #include <stdio.h>
+#include <string.h>
+
 
 // Global constants definition
 const int SYM_SIZE = 4;	// 3mt symbol size in bytes
@@ -13,33 +15,72 @@ const symbol EOF_SYM = 0xffffffff;	// 3mt end of file symbol
 const symbol MAGIC_SYM = 0x334d5421;	// 3mt file magic number
 
 
+void help()
+{
+		printf("Usage: shamitab [command] [files]\n\n"
+			"Available commands:\n\n"
+			"  view [tab.3mt]"
+			"			Displays specified tab in ASCII format.\n"
+			"  convert [ascii.txt] [tab.3mt]"
+			"		Converts ASCII tab into shamitab format.\n"
+			"\nExample:\n"
+			"  shamitab view ringo-bushi.3mt"
+			"		Displays the ringo-bushi.3mt tablature.\n"
+			);
+	return;
+}
+
+
 int main(int argc, char* argv[])
 {
 
 	// Constants
-	const int nargs = 1;	// Total number of arguments expected
+	const int nargs = 1;	// Minimum number of arguments expected
 
 
 	// Variables
+	char* command = NULL;
 	char* filename = NULL;
 	int err = 0;	// Error return variable
 
 
 	// Check if right number of arguments recieved
-	if(argc != nargs+1)
+	if(argc <= nargs)
 	{
-		printf("Error: expected %d argument(s) but got %d.\n", nargs, argc-1);
+		printf("Error: expected at least %d argument(s) but got %d.\n", nargs, argc-1);
 		return 1;
 	}
+
+	// Parsing arguments
+	command = argv[1];
+	filename = argv[2];
+
+	// Help routine
+	if(!strcmp(command, "help") || !strcmp(command, "h"))
+	{
+		help();
+	}
+
+	// Viewing routine
+	else if(!strcmp(command, "view") || !strcmp(command,"v"))
+	{
+		// Call viewing routine
+		err = view(filename);
+		if(err) return 1;
+	}
+
+	// Conversion routine
+	else if(!strcmp(command, "convert") || !strcmp(command, "c"))
+	{
+		printf("Not implemented yet.\n");
+	}
+
+	// Undefined command
 	else
 	{
-		// Parsing arguments
-		filename = argv[1];
+		printf("Error: unknown command. To see available commands: shamitab help.\n");
+		return 1;
 	}
-	
-	// Call viewing routine
-	err = view(filename);
-	if(err) return 1;
 
  	return 0;
 }
