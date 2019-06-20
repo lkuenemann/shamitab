959eb39917c5c5f81b2efeeb9e6227271f7ac12b
Luc Kuenemann <kuenemann.luc@protonmail.com>
2019-06-20 20:09:39 +0200

    Moved all previous tests to test/old Created new run-test.sh test script as a replacement

---
+++ test/run-tests.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+set -eu
+
+#How to call shamitab
+SHAMITAB='../shamitab'
+#List of all testing tabs (no file extension)
+TABLIST='all-effects all-fingers all-positions all-special triplets ringo-bushi-1 ringo-bushi-4 ringo-bushi-7 ringo-bushi-1to11'
+#Temporary output for testing
+OUTPUT='tmp_out'
+DATE=`date '+%Y-%m-%d-%H-%M-%S'`
+echo $DATE
+OUT_FILE_NAME='TEST_SUMMARY'
+SUMMARY=$DATE$OUT_FILE_NAME
+
+#Variables
+PASS=0
+TOTAL=0
+
+echo "Executed tests summary:" > $SUMMARY
+
+#For each tab in the list
+for tab in $TABLIST ; do
+
+	#Check that both the 3mt and txt tab files exist
+	if [ ! -f $tab.3mt ]; then
+		>&2 echo "ERROR: tab file $tab.3mt not found!";
+		exit 1;
+	fi
+	if [ ! -f $tab.txt ]; then
+		>&2 echo "ERROR: tab file $tab.txt not found!";
+		exit 1;
+	fi
+
+	#Viewing test
+
+	#Call Shamitab on the 3mt tab
+	$SHAMITAB view $tab.3mt > $OUTPUT
+	TOTAL=$((TOTAL+1))
+
+	#Compare to example txt tab
+	if diff --brief $OUTPUT $tab.txt > /dev/null;
+		then echo "PASS	view $tab" >> $SUMMARY; PASS=$((PASS+1))
+		else echo "FAIL	view $tab" >> $SUMMARY
+	fi
+
+	#Conversion test
+
+	#Call Shamitab on the txt tab
+	$SHAMITAB convert $tab.txt $OUTPUT
+	TOTAL=$((TOTAL+1))
+
+	#Compare to example 3mt tab
+	if diff --brief $OUTPUT $tab.3mt > /dev/null;
+		then echo "PASS	convert $tab" >> $SUMMARY; PASS=$((PASS+1))
+		else echo "FAIL	convert $tab" >> $SUMMARY
+	fi
+
+	#Full-circle (conversion then view) test
+
+	#Call shamitab on the txt tab
+	$SHAMITAB convert $tab.txt tmp_intermediate
+	#Call Shamitab on the 3mt tab
+	$SHAMITAB view tmp_intermediate > $OUTPUT
+	TOTAL=$((TOTAL+1))
+
+	#Compare to example txt tab
+	if diff --brief $OUTPUT $tab.txt > /dev/null;
+		then echo "PASS	full-circle $tab" >> $SUMMARY; PASS=$((PASS+1))
+		else echo "FAIL	full-circle $tab" >> $SUMMARY
+	fi
+
+	#Clean-up
+	rm tmp_intermediate
+	rm $OUTPUT
+
+done
+
+echo "$PASS/$TOTAL tests passed" >> $SUMMARY
+cat $SUMMARY
+
